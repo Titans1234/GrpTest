1ebbee6682b22151777747cf4c2becad6ec95e84
Mayankkkyadav <11997mayank@gmail.com>
2020-09-18 01:57:03 +0530

    Go

--- Airlines/src/app/login/login.component.ts
+++ Airlines/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 import { Component, OnInit } from '@angular/core';
+import { User} from '../user.module';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+ usn :string ;
+ psw: string ;
+ user :User ;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  search(usn:string){
+ // this.user =  this.service.search(usn);
   }
 
 }
